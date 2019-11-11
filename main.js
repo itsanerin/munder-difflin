ecaecb2f8dda4f5bc92c970a8b14a40ff5da712e
itsanerin <31420768+itsanerin@users.noreply.github.com>
2019-11-11 13:17:13 -0500

    added scroll-to-top button with fade-in / fade-out effect

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+scrollButton = document.getElementById('scroll-button')
 
+window.onscroll = function () {
+    scrollFunction()
+}
+
+scrollFunction = () => {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = "block"
+        scrollButton.style.opacity = "100%"
+    } else {
+        scrollButton.classList.toggle('fade')
+        scrollButton.style.opacity = "0%"
+    }
+}
+
+topFunction = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
