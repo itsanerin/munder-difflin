c3db0ffa7171b0a525fd7888a2369f7135980d43
Erin Brady <ebrady@nyc.yearup.org>
2019-11-12 13:10:34 -0500

    added product hover animations

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 scrollButton = document.getElementById('scroll-button')
 
-window.onscroll = function () {
-    scrollFunction()
+window.onscroll = function() {
+  scrollFunction()
 }
 
 scrollFunction = () => {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollButton.style.display = "block"
-        scrollButton.style.opacity = "100%"
-    } else {
-        scrollButton.classList.toggle('fade')
-        scrollButton.style.opacity = "0%"
-    }
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollButton.style.display = "block"
+    scrollButton.style.opacity = "100%"
+  } else {
+    scrollButton.classList.toggle('fade')
+    scrollButton.style.opacity = "0%"
+  }
 }
 
 topFunction = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+var item = document.getElementsByClassName('item')
+
+for (let i = 0; i < item.length; i++) {
+item[i].addEventListener("mouseover", () => {
+  item[i].style.transform = "scale(1.05)"
+})
+
+item[i].addEventListener("mouseleave", () => {
+  item[i].style.transform = "scale(1)"
+  behavior: 'smooth'
+})
 }
